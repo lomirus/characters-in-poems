3015d762b9e9a84476f65579f85f3949a5d5aa17
lumingrui <lomirus@qq.com>
2021-08-14 01:27:25 +0800

    feat: add dict data

--- index.ts
+++ index.ts
@@ -1,1 +1,7 @@
-console.log("Hello World!")
+import dict from "./dict.ts";
+
+function getPinyin(words: string): string[] {
+  return words.split("").map((char) => dict[char]);
+}
+
+console.log(getPinyin("测试"))
