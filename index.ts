eb9e196541385f92381b182f4e338594947d6bdb
lumingrui <lomirus@qq.com>
2021-08-14 02:30:56 +0800

    feat: some main calc functions

--- index.ts
+++ index.ts
@@ -1,7 +1,37 @@
 import dict from "./dict.ts";
 
-function getPinyin(words: string): string[] {
-  return words.split("").map((char) => dict[char]);
+interface Poem {
+    content: string[]
 }
 
-console.log(getPinyin("测试"))
+function getPinyin(words: string): string[] {
+    return words.split("").map((char) => dict[char]);
+}
+
+function getParagraphPower(paragraph: string, keyword: string): number {
+    const paragraphPinyin = getPinyin(paragraph.replace(/[，。！？]/g, ""));
+    const keywordPinyin = getPinyin(keyword);
+    return keywordPinyin.reduce(
+        (total, keyword) => paragraphPinyin.includes(keyword) ? total + 1 : total,
+        0,
+    );
+}
+
+function getPoemTotalPower(poem: Poem, keyword: string) {
+    return poem.content.reduce(
+        (total, paragraph) => total + getParagraphPower(paragraph, keyword),
+        0,
+    );
+}
+
+function getPoemMaxPower(poem: Poem, keyword: string) {
+    return poem.content.reduce(
+        (max, paragraph) => {
+            const cur = getParagraphPower(paragraph, keyword);
+            return cur > max ? cur : max;
+        },
+        0,
+    );
+}
+
+console.log(getPoemTotalPower({ content: ["清明时节雨纷纷，路上行人欲断魂"] }, "于小明"));
