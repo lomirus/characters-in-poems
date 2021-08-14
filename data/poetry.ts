c976c37549dea2c1640ee8208f495a7740d3ff66
lumingrui <lomirus@qq.com>
2021-08-14 22:53:51 +0800

    refactor: 添加 poetry.ts 以导出相关数据

---
+++ data/poetry.ts
@@ -0,0 +1,7 @@
+import shijing from './poetry/shijing.ts';
+import tangshi from './poetry/shijing.ts';
+
+export {
+    shijing,
+    tangshi
+}
--- index.ts
+++ index.ts
@@ -1,5 +1,4 @@
-//import tangshi from "./data/poetry/tangshi.ts";
-import shijing from "./data/poetry/shijing.ts";
+import * as poetry from "./data/poetry.ts";
 import { getBestMatchedResults } from './src/query.ts';
 import { highlightByBrackets } from './src/highlight.ts';
 import { Poem } from "./src/types.ts";
@@ -10,5 +9,5 @@ console.log(
             highlightByBrackets(
                 getBestMatchedResults(poems, keyword)[1], keyword
             )
-    )(shijing, "李沁钰")
+    )(poetry.shijing, "李沁钰")
 )
