6049efb24e9eab0a9c7f19f001fd781e6e53cc7b
lumingrui <lomirus@qq.com>
2021-08-14 21:11:03 +0800

    feat: 进一步封装函数

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ function getPinyin(words: string): string[] {
     return words.split("").map((char) => dict[char]);
 }
 
-function getParagraphPower(paragraph: string, keyword: string): number {
+function getParagraphCount(paragraph: string, keyword: string): number {
     const paragraphPinyin = getPinyin(paragraph.replace(/[，。！？]/g, ""));
     const keywordPinyin = getPinyin(keyword);
     return keywordPinyin.reduce(
@@ -15,20 +15,28 @@ function getParagraphPower(paragraph: string, keyword: string): number {
     );
 }
 
-function getPoemMaxPower(poem: Poem, keyword: string): [number, number] {
-    let maxPower = 0;
-    let maxPowerId = 0;
+function getPoemMaxCount(poem: Poem, keyword: string): [number, string] {
+    let maxCount = 0;
+    let maxCountParagraph = '';
     poem.content.forEach((paragraph, index) => {
-        const power = getParagraphPower(paragraph, keyword);
-        if (power > maxPower) {
-            maxPower = power;
-            maxPowerId = index;
+        const Count = getParagraphCount(paragraph, keyword);
+        if (Count > maxCount) {
+            maxCount = Count;
+            maxCountParagraph = poem.content[index];
         }
     })
-    return [maxPower, maxPowerId];
+    return [maxCount, maxCountParagraph];
+}
+
+function getBestMatchedParagraph(poems: Poem[], keyword: string): [number, string[]] {
+    let maxCounts: [number, string][] = [];
+    poems.forEach((poem) => maxCounts.push(getPoemMaxCount(poem, keyword)));
+    const maxCount = Math.max(...maxCounts.map(([count, _]) => count))
+    maxCounts = maxCounts.filter(([count, _]) => count === maxCount);
+    return [maxCount, maxCounts.map(([_, paragraph]) => paragraph)]
 }
 
 export {
-    getPoemMaxPower
+    getBestMatchedParagraph
 }
 
--- test.ts
+++ test.ts
@@ -1,10 +1,4 @@
 import tangshi from "./data/poetry/tangshi.ts";
-import {getPoemMaxPower} from './src/index.ts';
+import { getBestMatchedParagraph } from './src/index.ts';
 
-tangshi.forEach((poem) => {
-    const [maxPower, maxPowerId] = getPoemMaxPower(poem, "王小明");
-    if (maxPower > 2) {
-        console.log(maxPower)
-        console.log(poem.content[maxPowerId])
-    }
-});
+console.log(getBestMatchedParagraph(tangshi, "李沁钰"));
