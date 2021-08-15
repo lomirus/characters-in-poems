fdba39089826725f9b70bd5ceefe9c3a2f5d4444
lumingrui <lomirus@qq.com>
2021-08-15 11:42:23 +0800

    feat: 增加《楚辞》与《宋词三百首》

--- data/poetry.ts
+++ data/poetry.ts
@@ -1,11 +1,15 @@
 import shijing from './poetry/shijing.ts';
+import chuci from './poetry/chuci.ts';
 import tangshi from './poetry/tangshi.ts';
+import songci from './poetry/songci.ts';
 
 import { Poem } from "../src/types.ts";
 
 const poetry: Record<string, Poem[]> = {
     shijing,
-    tangshi
+    chuci,
+    tangshi,
+    songci,
 }
 
 export default poetry
