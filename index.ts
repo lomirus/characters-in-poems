401fe6b9b913e51fd394a2941a3d3b01a466d042
lumingrui <lomirus@qq.com>
2021-08-15 11:24:37 +0800

    feat: 指定格式化输出格式

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ import { parse } from "https://deno.land/std@0.104.0/flags/mod.ts";
 
 import poetry from "./data/poetry.ts";
 import { getMaxCountLines } from './src/query.ts';
-import { highlightByColor } from './src/highlight.ts';
+import { highlightByBrackets, highlightByColor } from './src/highlight.ts';
 import { Poem } from "./src/types.ts";
 
 
@@ -26,11 +26,36 @@ const specPoetry: Poem[] = (args.p ? args.p.split(",") : [])
         return [...poetries, ...poetry[poetryName]]
     }, []);
 
+const specFormat: string = (() => {
+    if (args.format) {
+        if (["color", "bracket"].includes(args.format)) {
+            return args.format
+        } else {
+            console.error(`Invalid format`);
+            Deno.exit()
+        }
+    }
+
+    if (args.f) {
+        if (["color", "bracket"].includes(args.f)) {
+            return args.f
+        } else {
+            console.error(`Invalid format`);
+            Deno.exit()
+        }
+    }
+
+    return "color"
+})();
+
+
 const [maxCount, maxCountLines] = getMaxCountLines(specPoetry, specWord)
 
 console.log(`最高符合字数：${maxCount}字\n`);
 
 maxCountLines.forEach(line => {
-    line.paragraph = highlightByColor(line.paragraph, specWord)
+    line.paragraph = specFormat === "color" ?
+        highlightByColor(line.paragraph, specWord) :
+        highlightByBrackets(line.paragraph, specWord)
     console.log(`${line.paragraph}%c - 《${line.title}》${line.author}%c`, "color: gray", "")
 })
