08cec57a670b1b68f23b6d56a5819bd3f12131ce
lumingrui <lomirus@qq.com>
2021-08-16 13:42:20 +0800

    refactor: 重命名诗集

--- data/poetry.ts
+++ data/poetry.ts
@@ -1,19 +1,19 @@
 import shijing from './poetry/shijing.ts';
 import chuci from './poetry/chuci.ts';
+import tangshi300 from './poetry/tangshi300.ts';
+import songci300 from './poetry/songci300.ts';
 import tangshi from './poetry/tangshi.ts';
 import songci from './poetry/songci.ts';
-import quantangshi from './poetry/quantangshi.ts';
-import quansongci from './poetry/quansongci.ts';
 
 import { Poem } from "../src/types.ts";
 
 const poetry: Record<string, Poem[]> = {
     shijing,
     chuci,
+    tangshi300,
+    songci300,
     tangshi,
-    songci,
-    quantangshi,
-    quansongci
+    songci
 }
 
 export default poetry
